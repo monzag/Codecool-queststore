be292a712e6d76774215173245e2dfb958376c0b
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-30 19:35:45 +0200

    Add constructor for mentor who does not have class assigned

--- models/account/Mentor.java
+++ models/account/Mentor.java
@@ -5,4 +5,9 @@ public class Mentor extends Codecooler {
 
     private Integer classId;
 
+    public Menotr(Integer id, Login login, Password password, Email email, String name, String surname) {
+        super(id, login, password, email, name, surname);
+        this.classId = null;
+    }
+
 }
