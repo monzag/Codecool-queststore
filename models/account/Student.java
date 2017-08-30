30e0da1f9a98812ae9238b0ee9219b5fa85280ff
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-30 19:50:35 +0200

    Add constructor for studnet assigned to class but unassigned to team

--- models/account/Student.java
+++ models/account/Student.java
@@ -12,6 +12,12 @@ public class Student extends Codecooler {
         this.teamId = null;
     }
 
+    public Mentor(Integer id, Login login, Password password, Email email, String name, String surname, Integer classId) {
+        super(id, login, password, email, name, surname);
+        this.classId = classId;
+        this.teamId = null;
+    }
+
     public Integer getClassId() {
         return this.classId;
     }
