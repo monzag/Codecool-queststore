161b86983cb30388f807f47580b4c1e42073bd6a
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-30 19:52:14 +0200

    Add constructor for student assigned to both class and team

--- models/account/Student.java
+++ models/account/Student.java
@@ -6,16 +6,22 @@ public class Student extends Codecooler {
     private Integer classId;
     private Integer teamId;
 
-    public Mentor(Integer id, Login login, Password password, Email email, String name, String surname) {
+    public Student(Integer id, Login login, Password password, Email email, String name, String surname) {
         super(id, login, password, email, name, surname);
         this.classId = null;
         this.teamId = null;
     }
 
-    public Mentor(Integer id, Login login, Password password, Email email, String name, String surname, Integer classId) {
+    public Student(Integer id, Login login, Password password, Email email, String name, String surname, Integer classId) {
         super(id, login, password, email, name, surname);
         this.classId = classId;
         this.teamId = null;
+    }
+
+    public Student(Integer id, Login login, Password password, Email email, String name, String surname, Integer classId, Integer teamId) {
+        super(id, login, password, email, name, surname);
+        this.classId = classId;
+        this.teamId = teamId;
     }
 
     public Integer getClassId() {
