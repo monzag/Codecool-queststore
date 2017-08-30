ef4300937220ae8bc55c2c290daf606ad65391d7
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-30 21:18:48 +0200

    Add temp Email class for test

---
+++ models/account/Email.java
@@ -0,0 +1,17 @@
+package models.account;
+
+
+public class Email {
+
+    private String value;
+
+    public Email(String value) {
+        this.value = value;
+    }
+
+    public String get() {
+        return this.value;
+    }
+
+
+}
