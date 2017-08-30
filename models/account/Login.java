a7a8bc73da34268bf326ad5f6a2742865002250f
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-30 21:19:08 +0200

    Add temporary Login class for testing

---
+++ models/account/Login.java
@@ -0,0 +1,17 @@
+package models.account;
+
+
+public class Login {
+
+    private String value;
+
+    public Login(String value) {
+        this.value = value;
+    }
+
+    public String get() {
+        return this.value;
+    }
+
+
+}
