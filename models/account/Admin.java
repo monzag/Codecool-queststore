3a71235f5a5a74039c53c4164bc9ba091aa8faaa
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-30 21:47:14 +0200

    Add new lane sign

--- models/account/Admin.java
+++ models/account/Admin.java
@@ -6,4 +6,6 @@ public class Admin extends Codecooler {
     public Admin(Integer id, Login login, Password password, Email email, String name, String surname) {
         super(id, login, password, email, name, surname);
     }
+
+
 }
