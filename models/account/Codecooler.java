0965d17d3b4175c96b48b4ca5a2cc97955a1d8f4
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-30 19:06:26 +0200

    Declare abstract class and its attribiutes

---
+++ models/account/Codecooler.java
@@ -0,0 +1,17 @@
+package models.account;
+
+
+public abstract class Codecooler {
+
+    protected Integer ID;
+    protected Login login;
+    protected Password password;
+    protected Email email;
+    protected String name;
+    protected String surname;
+
+    public Codecooler() {
+
+    }
+
+}
