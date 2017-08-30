14dfb3b99a0cb4203b92e64249bf98a538cc1417
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-30 21:19:35 +0200

    Add temporary Password class for testing

---
+++ models/account/Password.java
@@ -0,0 +1,17 @@
+package models.account;
+
+
+public class Password {
+
+    private String value;
+
+    public Password(String value) {
+        this.value = value;
+    }
+
+    public String get() {
+        return this.value;
+    }
+
+
+}
