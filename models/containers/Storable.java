c1299ac37dfcedb64de9c07174ed2c047f02f89c
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-31 09:55:32 +0200

    Change add to return boolean

--- models/containers/Storable.java
+++ models/containers/Storable.java
@@ -7,7 +7,9 @@ public interface Storable<T> {
 
     public void load();
     public void save();
-    public void add(T storableObject);
+    // add returns false if object already was on list
+    public boolean add(T storableObject);
+    // remove returns false if there was no such object on list
     public boolean remove(T storableObject);
     public ArrayList<T> getAll();
     public Iterator getIterator();
