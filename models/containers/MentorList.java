3ed832b46bc4d2b204063962be63ab0cf64ae082
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-31 10:34:56 +0200

    Implemented all methods based on AdminList code

--- models/containers/MentorList.java
+++ models/containers/MentorList.java
@@ -1,7 +1,6 @@
 package models.containers;
 
-
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import models.account.Mentor;
@@ -9,5 +8,38 @@ import models.account.Mentor;
 
 public class MentorList<Mentor> implements Storable {
 
-    
+    private ArrayList<Mentor> mentors;
+
+    public MentorList() {
+        this.mentors = new ArrayList<Mentor>();
+    }
+
+    public ArrayList<Mentor> getAll() {
+        return this.mentors;
+    }
+
+    public void load() {
+        //TODO
+    }
+
+    public void save() {
+        //TODO
+    }
+
+    public boolean add(Mentor mentor) {
+        if (!this.mentors.contains(mentor)) {
+            mentors.add(mentor);
+            return true;
+        }
+        return false;
+    }
+
+    public Iterator getIterator() {
+        return this.mentors.iterator();
+    }
+
+    public boolean remove(Mentor mentor) {
+        return this.mentors.remove(mentor);
+    }
+
 }
