053dacee719ba2f443c45255e0607fe26f898519
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-31 08:57:26 +0200

    Add methods signatures

--- models/containers/Storable.java
+++ models/containers/Storable.java
@@ -3,4 +3,10 @@ package models.containers;
 
 public interface Storable<T> {
 
+    public void load();
+    public void save();
+    public void add(T storableObject);
+    public boolean remove(T storableObject);
+    public ArrayList<T> getAll();
+
 }
