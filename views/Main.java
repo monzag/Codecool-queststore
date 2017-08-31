8a30309cff0a9730457f47c62a98ae4665b0f619
wutismyname <plebejoosh@gmail.com>
2017-08-31 19:59:16 +0200

    Add main()

--- views/Main.java
+++ views/Main.java
@@ -1,10 +1,15 @@
 package views;
 
 import java.util.ArrayList;
+import controllers.AdminController;
+import controllers.AppController;
+import models.account.Codecooler;
+import models.account.Admin;
 
 public class Main {
     public static void main(String[] args) {
-        AdminView.viewOptions();
-        AdminView.showAllMentors();
+        Admin admin = (Admin)AppController.login();
+        AdminController user = new AdminController(admin);
+        user.menu();
     }
 }
