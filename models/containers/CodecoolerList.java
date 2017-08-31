a50ea30422a947998a171e14b94744bcf350f0a0
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-31 10:40:43 +0200

    Redone common code from MentorList and AdminList into a abstract CodecoolerList class

---
+++ models/containers/CodecoolerList.java
@@ -0,0 +1,39 @@
+package models.containers;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public abstract class CodecoolerList<T> implements Storable {
+
+    private ArrayList<T> codecoolers;
+
+    public CodecoolersList() {
+        this.codecoolers = new ArrayList<T>();
+    }
+
+    public ArrayList<T> getAll() {
+        return this.codecoolers;
+    }
+
+    public abstract void load();
+
+    public abstract void save();
+
+    public boolean add(T codecooler) {
+        if (!this.codecoolers.contains(codecooler)) {
+            codecoolers.add(codecooler);
+            return true;
+        }
+        return false;
+    }
+
+    public Iterator getIterator() {
+        return this.codecoolers.iterator();
+    }
+
+    public boolean remove(T codecooler) {
+        return this.codecoolers.remove(codecooler);
+    }
+
+}
