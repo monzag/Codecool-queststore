dd129059450a5ef76a0c23150a7f4f1f7377f570
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-08-31 13:31:40 +0200

    Add final static attributes that describe login length

--- models/accountdata/Login.java
+++ models/accountdata/Login.java
@@ -1,6 +1,8 @@
 package models.accountdata;
 
 public class Login extends AccountData {
+    private static final Integer LOGINMAXLEN = 20;
+    private static final Integer LOGINMINLEN = 5;
 
     public Login() {
 
@@ -12,7 +14,7 @@ public class Login extends AccountData {
 
     public static boolean isValid(String value){
         boolean isUnique = isUnique(value);
-        boolean isLengthValid = isLengthValid(value, 5, 20);
+        boolean isLengthValid = isLengthValid(value, LOGINMINLEN, LOGINMAXLEN);
 
         if (isLengthValid && isUnique) {
             return true;
