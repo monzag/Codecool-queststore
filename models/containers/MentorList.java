9ad61c02cfbf61b8913ecabfbaac98cbf7ce6aa4
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-31 10:29:51 +0200

    Add file with class MentorList and its declaration

---
+++ models/containers/MentorList.java
@@ -0,0 +1,13 @@
+package models.containers;
+
+
+import java.util.LinkedList;
+import java.util.Iterator;
+
+import models.account.Mentor;
+
+
+public class MentorList<Mentor> implements Storable {
+
+    
+}
