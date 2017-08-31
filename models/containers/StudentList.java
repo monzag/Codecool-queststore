c0ddc0a6a4b1ad48e29529c50c51aa0ad8190281
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-31 10:47:38 +0200

    Add StudentList class file

---
+++ models/containers/StudentList.java
@@ -0,0 +1,17 @@
+package models.containers;
+
+import models.account.Student;
+
+
+public class MentorList<Student> extends CodecoolerList {
+
+    public void load() {
+        //TODO
+    }
+
+    public void save() {
+        //TODO
+    }
+
+
+}
