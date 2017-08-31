4e00da4d53a0fa0af5be5febb87ea822f2e5ce6f
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-08-31 13:35:48 +0200

    Add method and set values for checking if password length is valid

---
+++ models/accountdata/Password.java
@@ -0,0 +1,21 @@
+package models.accountdata;
+
+public class Password extends AccountData {
+    private static final Integer PASSMAXLEN = 50;
+    private static final Integer PASSMINLEN = 7;
+
+    public Password(){
+    }
+
+    public Password(String value) {
+        this.value = value;
+    }
+
+    public static boolean isValid(String value){
+        boolean isLengthValid = isLengthValid(value, LOGINMINLEN, LOGINMAXLEN);
+
+        if (isLengthValid){
+            return true;
+        }
+    }
+}
