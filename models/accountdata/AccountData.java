fbc89395550caa40009066e6ddb81cce05687c8d
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-08-31 10:38:20 +0200

    Implement abstract class AccountData

---
+++ models/accountdata/AccountData.java
@@ -0,0 +1,22 @@
+package models.accountdata;
+
+public abstract class AccountData {
+    private String value;
+
+    public AccountData() {
+    }
+
+    public AccountData(String value) {
+        this.value = value;
+    }
+
+    public abstract boolean isValid();
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
