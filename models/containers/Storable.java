659b1abf364a2d3f11465fab3dfe7a1102e5826a
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-31 09:46:03 +0200

    Add getIterator method

--- models/containers/Storable.java
+++ models/containers/Storable.java
@@ -8,5 +8,6 @@ public interface Storable<T> {
     public void add(T storableObject);
     public boolean remove(T storableObject);
     public ArrayList<T> getAll();
+    public Iterator getIterator();
 
 }
