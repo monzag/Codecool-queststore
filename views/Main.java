c06d0c879b462420dd1381450020d9adec7dd3d4
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-08-31 18:33:43 +0200

    Test main added

---
+++ views/Main.java
@@ -0,0 +1,10 @@
+package views;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        AdminView.viewOptions();
+        AdminView.showAllMentors();
+    }
+}
