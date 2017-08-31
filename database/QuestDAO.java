5f70bdca3bee723b9f1bf74eccad9b1cd4f829c7
wutismyname <plebejoosh@gmail.com>
2017-08-31 11:02:32 +0200

    Add load function to QuestDAO

---
+++ database/QuestDAO.java
@@ -0,0 +1,55 @@
+package database;
+
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class QuestDAO implements DAO {
+
+    private final String FILEPATH = "database/quests/";
+
+    public QuestDAO() {
+    }
+
+    public Quest load(String id) {
+        try (BufferedReader br = new BufferedReader(new FileReader(FILEPATH + id + ".txt"))) {
+            Quest quest = new Quest();
+            String rId = br.readline();
+            String rName = br.readline();
+            String rDescription = br.readline();
+            String rReward = br.readline();
+
+            if (rId.startswith("ID: ")) {
+                quest.setId(rId.substring("ID: ".length()));
+            }
+            else {
+                return null;
+            }
+            if (rName.startswith("NAME: ")) {
+                quest.setLogin(rName.substring("NAME: ".length()));
+            }
+            else {
+                return null;
+            }
+            if (rDescription.startswith("DESCRIPTION: ")) {
+                quest.setPassword(rDescription.substring("DESCRIPTION: ".length()));
+            }
+            else {
+                return null;
+            }
+            if (rReward.startswith("REWARD: ")) {
+                quest.setPassword(rReward.substring("REWARD: ".length()));
+            }
+            else {
+                return null;
+            }
+            return quest;
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        }
+    }
+
+
+
+}
