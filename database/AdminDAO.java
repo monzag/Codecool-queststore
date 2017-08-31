c50f9f08d2b479ddb47c6a0cd9e06a3f6bafba1d
wutismyname <plebejoosh@gmail.com>
2017-08-31 12:57:38 +0200

    Add load() method to AdminDAO

---
+++ database/AdminDAO.java
@@ -0,0 +1,65 @@
+package database;
+
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class AdminDAO implements DAO {
+
+    private final String FILEPATH = "database/mentors/";
+
+    public AdminDAO() {
+    }
+
+    public Admin load(String login) {
+        try (BufferedReader br = new BufferedReader(new FileReader(FILEPATH + login + ".txt"))) {
+            Admin admin = new Admin();
+            String rId = br.readline();
+            String rLogin = br.readline();
+            String rPassword = br.readline();
+            String rEmail = br.readline();
+            String rName = br.readline();
+            String rSurname = br.readline();
+
+            if (rId.startswith("ID: ")) {
+                admin.setId(rId.substring("ID: ".length()));
+            }
+            else {
+                return null;
+            }
+            if (rLogin.startswith("LOGIN: ")) {
+                admin.setLogin(new Login(rLogin.substring("LOGIN: ".length())));
+            }
+            else {
+                return null;
+            }
+            if (rPassword.startswith("PASSWORD: ")) {
+                admin.setPassword(new Password(rPassword.substring("PASSWORD: ".length())));
+            }
+            else {
+                return null;
+            }
+            if (rEmail.startswith("EMAIL: ")) {
+                admin.setEmail(new Email(rEmail.substring("EMAIL: ".length())));
+            }
+            else {
+                return null;
+            }
+            if (rName.startswith("NAME: ")) {
+                admin.setName(rName.substring("NAME: ".length()));
+            }
+            else {
+                return null;
+            }
+            if (rSurname.startswith("SURNAME: ")) {
+                mentor.setSurname(rId.substring("SURNAME: ".length()));
+            }
+            else {
+                return null;
+            }
+            return admin;
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        }
+}
