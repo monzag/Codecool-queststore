6bb8e8e5733b7e65aa67405dd2d3723a3d708004
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-08-31 13:46:45 +0200

    Add createMentor method

--- controllers/AdminController.java
+++ controllers/AdminController.java
@@ -2,6 +2,11 @@ package controllers;
 
 import models.account.Admin;
 import models.account.Mentor;
+
+import models.accountdata.Login;
+import models.accountdata.Password;
+import models.accountdata.Email;
+
 import models.containers.MentorList;
 
 
@@ -24,6 +29,25 @@ public class AdminController {
         }
         // reports false if failed
         return false;
+    }
+
+    public void createMentor() {
+        Integer id;
+        Login login;
+        Password password;
+        Email email;
+        String name;
+        String surname;
+        Mentor mentor;
+
+        name = CodecoolerView.getString();
+        surname = CodecoolerView.getString();
+        login = new Login(CodecoolerView.getLogin());
+        password = new Password(CodecoolerView.getLogin());
+        email = new Email(CodecoolerView.getEmail());
+
+        CodecoolerView.reportResult(this.addMentor(mentor))
+
     }
 
 }
