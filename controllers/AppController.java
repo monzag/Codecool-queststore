c387dd3eff9ff52fe4d88c20b6ef9bc50135b66e
wutismyname <plebejoosh@gmail.com>
2017-09-01 09:55:14 +0200

    Add password checking to login

--- controllers/AppController.java
+++ controllers/AppController.java
@@ -18,23 +18,35 @@ public class AppController {
         MentorDAO mentorData = new MentorDAO();
         AdminDAO adminData = new AdminDAO();
 
-        boolean successful = false;
+        boolean isLogging = true;
 
-        while (!successful) {
+        while (isLogging) {
 
             String login = CodecoolerView.getString("login");
+            String password = CodecoolerView.getString("password");
 
-            if (!adminData.load(login).equals(null)) {
+            if (adminData.load(login) != null) {
                 Admin admin = adminData.load(login);
-                return admin;
+                if (admin.getPassword().getValue().equals(password)) {
+                    return admin;
+                }
             }
-            else if (!mentorData.load(login).equals(null)) {
+            else if (mentorData.load(login) != null) {
                 Mentor mentor = mentorData.load(login);
-                return mentor;
+                if (mentor.getPassword().getValue().equals(password)) {
+                    return mentor;
+                }
             }
-            else if (!studentData.load(login).equals(null)) {
+            else if (studentData.load(login) != null) {
                 Student student = studentData.load(login);
-                return student;
+                if (student.getPassword().getValue().equals(password)) {
+                    return student;
+                }
+            }
+            System.out.println("Wrong login data. Do you want to try again?");
+            String tryAgain = CodecoolerView.getString("Y or anything else");
+            if (!tryAgain.equalsIgnoreCase("y")) {
+                isLogging = false;
             }
         }
         return null;
