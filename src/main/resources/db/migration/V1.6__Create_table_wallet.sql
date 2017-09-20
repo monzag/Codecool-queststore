5cac948ccbbf7c5782c46efe866efef365062d87
wutismyname <plebejoosh@gmail.com>
2017-09-20 15:26:36 +0200

    Fix foreign key in create table wallet

--- src/main/resources/db/migration/V1.6__Create_table_wallet.sql
+++ src/main/resources/db/migration/V1.6__Create_table_wallet.sql
@@ -1,5 +1,6 @@
-CREATE TABLE wallet (
-    coolcoins INTEGER NOT NULL,
-    balance INTEGER NOT NULL,
-    FOREIGN KEY('student_login') REFERENCES Student('login') ON DELETE SET NULL;
+CREATE TABLE IF NOT EXISTS `wallet` (
+    `student_login` TEXT NOT NULL,
+	`coolcoins` INTEGER NOT NULL,
+	`balance`	INTEGER NOT NULL,
+	FOREIGN KEY(`student_login`) REFERENCES `student`(`login`) ON DELETE SET NULL
 );
