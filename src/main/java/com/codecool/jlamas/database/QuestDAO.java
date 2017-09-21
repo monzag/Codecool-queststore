dde2b0198e286fa690fe3c583598f3049e35bb45
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-09-21 14:11:02 +0200

    Implement method that insert new record into the table

--- src/main/java/com/codecool/jlamas/database/QuestDAO.java
+++ src/main/java/com/codecool/jlamas/database/QuestDAO.java
@@ -7,72 +7,23 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class QuestDAO implements DAO {
+public class QuestDAO extends AbstractDAO {
+    public QuestDAO() {
 
-    // private final String FILEPATH = "database/quests/";
+    }
 
-    // public QuestDAO() {
-    // }
-
-    // public Quest load(String id) {
-    //     try (BufferedReader br = new BufferedReader(new FileReader(FILEPATH + id + ".txt"))) {
-    //         Quest quest = new Quest();
-    //         String rId = br.readLine();
-    //         String rName = br.readLine();
-    //         String rDescription = br.readLine();
-    //         String rReward = br.readLine();
-
-    //         if (rId.startsWith("ID: ")) {
-    //             quest.setId(rId.substring("ID: ".length()));
-    //         }
-    //         else {
-    //             return null;
-    //         }
-    //         if (rName.startsWith("NAME: ")) {
-    //             quest.setLogin(rName.substring("NAME: ".length()));
-    //         }
-    //         else {
-    //             return null;
-    //         }
-    //         if (rDescription.startsWith("DESCRIPTION: ")) {
-    //             quest.setDescription(rDescription.substring("DESCRIPTION: ".length()));
-    //         }
-    //         else {
-    //             return null;
-    //         }
-    //         if (rReward.startsWith("REWARD: ")) {
-    //             quest.setReward(Integer.parseInt(rReward.substring("REWARD: ".length())));
-    //         }
-    //         else {
-    //             return null;
-    //         }
-    //         return quest;
-    //     } catch (IOException e) {
-    //         System.out.println("File not found.");
-    //     }
-    // }
-
-    // public ArrayList<Quest> loadAll() {
-    //     ArrayList<Quest> questList = new ArrayList<>();
-
-    //     File[] files = new File(FILEPATH).listFiles();
-
-    //     for (File file : files) {
-    //         int dotPosition = file.getName().lastIndexOf(".");
-    //         questList.add(load(file.getName().substring(0, dotPosition)));
-    //     }
-    //     return questList;
-    // }
-
-    // public void save(Quest quest) {
-    //     try (FileWriter fw = new FileWriter(FILEPATH + quest.getId() + ".txt")) {
-    //         fw.write("ID: " + quest.getId());
-    //         fw.write("NAME: " + quest.getName());
-    //         fw.write("DESCRIPTION: " + quest.getDescription());
-    //         fw.write("REWARD: " + quest.getReward());
-    //     } catch (IOException e) {
-    //         System.out.println("Filepath not found.");
-    //     }
-    // }
-
+    public boolean insertQuest(String name, String description, Integer reward) {
+        try {
+            PreparedStatement prepStmt = conn.prepareStatement(
+                    "INSERT INTO quest(name, description, reward) VALUES (?, ?, ?);");
+            prepStmt.setString(1, name);
+            prepStmt.setString(2, description);
+            prepStmt.setInteger(3, reward);
+            prepStmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error");
+            return false;
+        }
+        return true;
+    }
 }
