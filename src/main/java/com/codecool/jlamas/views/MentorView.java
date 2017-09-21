3300185783885d7a8df4e8cce436e1ccd8bf6dc0
monzag <zaganiacz.m@gmail.com>
2017-09-21 18:14:07 +0200

    Add displayAll

--- src/main/java/com/codecool/jlamas/views/MentorView.java
+++ src/main/java/com/codecool/jlamas/views/MentorView.java
@@ -1,6 +1,9 @@
 package com.codecool.jlamas.views;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+
+import com.codecool.jlamas.exceptions.InvalidUserDataException;
+import com.codecool.jlamas.models.account.Mentor;
 
 
 public class MentorView extends CodecoolerView {
@@ -9,5 +12,30 @@ public class MentorView extends CodecoolerView {
         super();
     }
 
-    
+    public String getAttribute() {
+        String[] attributes = {"name", 
+                               "surname",
+                               "email",
+                               "password"};
+        printMenu(attributes);
+
+        System.out.println("\nYour choice: 1");
+        try {
+            String name = getSurname();
+            return name;
+
+        } catch (InvalidUserDataException e) {
+
+        }
+
+    }
+
+    public void displayAll(ArrayList<Mentor> mentors) {
+        System.out.println("\nUsers:");
+        Integer number = 1;
+        for (Mentor mentor : mentors) {
+            System.out.println(number + ". " + mentor);
+            number++;
+        }
+    }
 }
