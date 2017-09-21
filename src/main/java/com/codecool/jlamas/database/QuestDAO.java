59f5e694591d0a9b213423fac6779297a14b8c9a
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-09-21 16:00:58 +0200

    Fix method that inserts new quest into table

--- src/main/java/com/codecool/jlamas/database/QuestDAO.java
+++ src/main/java/com/codecool/jlamas/database/QuestDAO.java
@@ -1,8 +1,8 @@
 package com.codecool.jlamas.database;
 
-import java.sql.SQLException;
+import java.sql.*;
 
-public class QuestDAO extends AbstractDAO {
+public class QuestDAO {
     public QuestDAO() {
 
     }
@@ -10,36 +10,36 @@ public class QuestDAO extends AbstractDAO {
     public void insertQuest(String name, String description, Integer reward) {
         String sql = "INSERT INTO quest(name, description, reward) VALUES (?, ?, ?);";
 
-        try (Connection conn = this.connect();
-                PreparedStatement pstmt = connection.prepareStatement(sql)) {
-
-            prepStmt.setString(1, name);
-            prepStmt.setString(2, description);
-            prepStmt.setInt(3, reward);
-            prepStmt.executeUpdate();
-
-        } catch (SQLException e) {
-            System.out.println("Error");
-        }
-    }
-
-    public void update(int id, String name, String description, Integer reward) {
-        String sql = "UPDATE quest SET name = ? , "
-                + "description = ? "
-                + "reward = ? "
-                + "WHERE id = ?";
-
-        try (Connection connection = this.connect();
-                PreparedStatement pstmt = connection.prepareStatement(sql)) {
+        try (Connection c = ConnectDB.connect();
+                PreparedStatement pstmt = c.prepareStatement(sql);) {
 
             pstmt.setString(1, name);
             pstmt.setString(2, description);
             pstmt.setInt(3, reward);
-            pstmt.setInt(4, id);
-
             pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println("Error");
+
+        } catch (ClassNotFoundException|SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
+
+    // public void update(int id, String name, String description, Integer reward) {
+    //     String sql = "UPDATE quest SET name = ? , "
+    //             + "description = ? "
+    //             + "reward = ? "
+    //             + "WHERE id = ?";
+    //
+    //     try (Connection connection = this.connect();
+    //             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+    //
+    //         pstmt.setString(1, name);
+    //         pstmt.setString(2, description);
+    //         pstmt.setInt(3, reward);
+    //         pstmt.setInt(4, id);
+    //
+    //         pstmt.executeUpdate();
+    //     } catch (SQLException e) {
+    //         System.out.println("Error");
+    //     }
+    // }
 }
