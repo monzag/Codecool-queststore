d455d0a58de7f53cad215e03e422cf31241b3077
monzag <zaganiacz.m@gmail.com>
2017-09-21 16:41:27 +0200

    Add addMentor

--- src/main/java/com/codecool/jlamas/controllers/MentorController.java
+++ src/main/java/com/codecool/jlamas/controllers/MentorController.java
@@ -1,15 +1,34 @@
 package com.codecool.jlamas.controllers;
 
+import com.codecool.jlamas.exceptions.InvalidUserDataException;
 import com.codecool.jlamas.models.account.Mentor;
+import com.codecool.jlamas.models.accountdata.Login;
+import com.codecool.jlamas.models.accountdata.Mail;
+import com.codecool.jlamas.models.accountdata.Password;
+import com.codecool.jlamas.views.MentorView;
 
 public class MentorController {
 
+    private MentorView mentorView = new MentorView();
+
     public MentorController() {
+        
     }
 
     public void addMentor() {
-        // getData
-        // MentorDAO - insert new
+        try {
+            String name = mentorView.getName();
+            String surname = mentorView.getSurname();
+            Mail email = mentorView.getMail();
+
+            Login login = new Login();
+            Password password = new Password();
+            Mentor mentor = new Mentor(login, password, email, name, surname);
+            // MentorDAO - insert new
+
+        } catch (InvalidUserDataException e) {
+
+        }
     }
 
     public void editMentor() {
