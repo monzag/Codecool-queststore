287bbe2e331031f02ad49c366eba57619d2b1270
monzag <zaganiacz.m@gmail.com>
2017-09-21 22:44:26 +0200

    Add error message to removeMentor

--- src/main/java/com/codecool/jlamas/controllers/MentorController.java
+++ src/main/java/com/codecool/jlamas/controllers/MentorController.java
@@ -50,7 +50,7 @@ public class MentorController {
             mentorDao.delete(mentor);
 
         } catch (IndexOutOfBoundsException e) {
-
+            mentorView.printIndexError();
         }
     }
 
