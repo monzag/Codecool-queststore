462caf167e2963d6c8574313ba87277154b50098
monzag <zaganiacz.m@gmail.com>
2017-09-21 19:49:21 +0200

    Add displayAll

--- src/main/java/com/codecool/jlamas/views/StudentView.java
+++ src/main/java/com/codecool/jlamas/views/StudentView.java
@@ -1,8 +1,21 @@
 package com.codecool.jlamas.views;
 
+import java.util.ArrayList;
+
+import com.codecool.jlamas.models.account.Student;
+
 public class StudentView extends CodecoolerView {
 
     public StudentView() {
         super();
+    }
+
+    public void displayAll(ArrayList<Student> students) {
+        System.out.println("\nUsers:");
+        Integer number = 1;
+        for (Student student : students) {
+            System.out.println(number + ". " + student);
+            number++;
+        }
     }
 }
