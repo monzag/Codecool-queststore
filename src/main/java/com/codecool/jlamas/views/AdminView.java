4e711bd6556cdccd7cccc36c82c90db045967044
monzag <zaganiacz.m@gmail.com>
2017-09-21 11:34:25 +0200

    Remove viewOptions

--- src/main/java/com/codecool/jlamas/views/AdminView.java
+++ src/main/java/com/codecool/jlamas/views/AdminView.java
@@ -6,14 +6,6 @@ import com.codecool.jlamas.models.account.Mentor;
 
 public class AdminView extends CodecoolerView {
 
-    public static void viewOptions() {
-        String[] options = {"1. View all mentors", "2. Create mentor", "0. Exit"};
-
-        for (String option : options) {
-            System.out.println(option);
-        }
-    }
-
     public static void showAllMentors() {
         MentorDAO mentors = new MentorDAO();
         ArrayList<Mentor> mentorList = mentors.loadAll();
