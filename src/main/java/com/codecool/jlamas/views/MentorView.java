e79e85cefdbb5fc3265517150efe6b5375b89fa3
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-09-21 08:59:47 +0200

    Add file with Mentor view

---
+++ src/main/java/com/codecool/jlamas/views/MentorView.java
@@ -0,0 +1,11 @@
+package com.codecool.jlamas.views;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class MentorView extends CodecoolerView {
+
+    public static final ArrayList<String> menu = {};
+
+}
