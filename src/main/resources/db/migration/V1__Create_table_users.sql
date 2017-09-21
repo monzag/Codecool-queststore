f0824cee7d09fe52574ffa36e40acf91c333b14e
wutismyname <plebejoosh@gmail.com>
2017-09-21 16:01:36 +0200

    Change users to user

--- src/main/resources/db/migration/V1__Create_table_users.sql
+++ src/main/resources/db/migration/V1__Create_table_users.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `user` (
 	`login`	TEXT PRIMARY KEY UNIQUE NOT NULL,
 	`email`	TEXT NOT NULL,
 	`name`	TEXT NOT NULL,
 	`surname`	TEXT NOT NULL
 );
-INSERT INTO `users` VALUES ('admin', 'admin@admin.com','Jerzy','Mardaus');
+INSERT INTO `user` VALUES ('admin', 'admin@admin.com','Jerzy','Mardaus');
 
