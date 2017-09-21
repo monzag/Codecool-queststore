89818d57e94460579038636470ee685a8b2b9d6a
monzag <zaganiacz.m@gmail.com>
2017-09-21 19:35:50 +0200

    Add addStudent

--- src/main/java/com/codecool/jlamas/controllers/StudentController.java
+++ src/main/java/com/codecool/jlamas/controllers/StudentController.java
@@ -1,8 +1,38 @@
 package com.codecool.jlamas.controllers;
 
+import com.codecool.jlamas.exceptions.InvalidUserDataException;
+import com.codecool.jlamas.models.account.Student;
+import com.codecool.jlamas.models.accountdata.Login;
+import com.codecool.jlamas.models.accountdata.Mail;
+import com.codecool.jlamas.models.accountdata.Password;
+import com.codecool.jlamas.models.accountdata.Wallet;
+
 public class StudentController {
 
     public StudentController() {
-        
+
     }
+
+    public void displayAll() {
+        ;
+    }
+
+    public void addStudent() {
+        try {
+            String name = studentView.getName();
+            String surname = studentView.getSurname();
+            Mail email = studentView.getMail();
+
+            Login login = new Login("student");
+            Password password = new Password("student");
+            Wallet wallet = new Wallet();
+            Student student = new Student(login, password, email, name, surname, wallet);
+            studentDao.insert(student);
+
+        } catch (InvalidUserDataException e) {
+
+        }
+    }
+
+    
 }
