8f141c4af2d3ffbd06305d0e529081bf2990f54e
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-09-21 09:07:29 +0200

    Add printMenu method

--- src/main/java/com/codecool/jlamas/views/MentorView.java
+++ src/main/java/com/codecool/jlamas/views/MentorView.java
@@ -9,7 +9,20 @@ public class MentorView extends CodecoolerView {
     public static final ArrayList<String> menu = {};
 
     public MentorView() {
-        
+
     }
+
+    public void printMenu() {
+        String output;
+
+        output = "\nMENU";
+        for (int i = 0; i < menu.length; i++) {
+            output += String.format("  %d) %s.\n", i+1, menu[i]);
+        }
+        output += "  0) Exit.\n";
+
+        System.out.println(output);
+    }
+
 
 }
