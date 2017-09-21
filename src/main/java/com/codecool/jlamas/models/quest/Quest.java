e7c8b734b0d7e7d95dd99a37631ac478a65df96e
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-09-21 11:21:45 +0200

    Create class quest model and add its attributes

---
+++ src/main/java/com/codecool/jlamas/models/quest/Quest.java
@@ -0,0 +1,9 @@
+package com.codecool.jlamas.models.quest;
+
+public class Quest {
+    private String name;
+    private String description;
+    private Integer reward;
+    private boolean isDone;
+
+}
