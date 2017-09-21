5169327fd15e6d07b3b3bf373ee3af284de5c761
wutismyname <plebejoosh@gmail.com>
2017-09-21 19:47:24 +0200

    Add getAdmin() to UserDAO

--- src/main/java/com/codecool/jlamas/database/UserDAO.java
+++ src/main/java/com/codecool/jlamas/database/UserDAO.java
@@ -2,6 +2,8 @@ package com.codecool.jlamas.database;
 
 import java.sql.*;
 
+import com.codecool.jlamas.models.account.Admin;
+
 public class UserDAO {
 
     public UserDAO() {}
@@ -17,6 +19,28 @@ public class UserDAO {
             if (rs.next()) {
                 String userType = rs.getString("type");
                 return userType;
+            }
+
+        } catch (ClassNotFoundException|SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Admin getAdmin(String login) {
+
+        String query = "SELECT * FROM user WHERE user.login = " + login + ";";
+
+        try (Connection c = ConnectDB.connect();
+             Statement stmt = c.createStatement()) {
+
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                String name = rs.getString("name");
+                String surname = rs.getString("surname");
+                String email = rs.getString("email");
+                Admin admin = new Admin(login, name, surname, email);
+                return admin;
             }
 
         } catch (ClassNotFoundException|SQLException e) {
