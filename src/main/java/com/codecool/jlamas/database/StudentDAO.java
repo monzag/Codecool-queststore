0f83f30cf80b2bd833b01f351e51c351cf6755bf
monzag <zaganiacz.m@gmail.com>
2017-09-21 19:59:15 +0200

    Add empty methods

--- src/main/java/com/codecool/jlamas/database/StudentDAO.java
+++ src/main/java/com/codecool/jlamas/database/StudentDAO.java
@@ -1,8 +1,26 @@
 package com.codecool.jlamas.database;
 
+import com.codecool.jlamas.models.account.Student;
+
 public class StudentDAO {
 
     public StudentDAO() {
+    }
+
+    public void requestAll() {
+        ;
+    }
+
+    public void delete(Student student) {
+        ;
+    }
+
+    public void insert(Student student) {
+        ;
+    }
+
+    public void update(Student student) {
+        ;
     }
 
 }
