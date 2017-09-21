e29a84053ffc0dcfa14d40ba93fc14c862c77133
monzag <zaganiacz.m@gmail.com>
2017-09-21 19:50:08 +0200

    Add getAttribute

--- src/main/java/com/codecool/jlamas/views/StudentView.java
+++ src/main/java/com/codecool/jlamas/views/StudentView.java
@@ -2,6 +2,7 @@ package com.codecool.jlamas.views;
 
 import java.util.ArrayList;
 
+import com.codecool.jlamas.exceptions.InvalidUserDataException;
 import com.codecool.jlamas.models.account.Student;
 
 public class StudentView extends CodecoolerView {
@@ -17,5 +18,26 @@ public class StudentView extends CodecoolerView {
             System.out.println(number + ". " + student);
             number++;
         }
+    }
+
+    public String getAttribute() {
+        String attribute = null;
+        String[] attributes = {"name", 
+                               "surname",
+                               "email",
+                               "password"};
+        printMenu(attributes);
+
+        // Demo:
+        System.out.println("\nYour choice: 1");
+        try {
+            attribute = getSurname();
+
+        } catch (InvalidUserDataException e) {
+
+        }
+
+        return attribute;
+
     }
 }
