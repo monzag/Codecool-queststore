0e5c905fe6fd6fc6b622831972dffca58802396d
wutismyname <plebejoosh@gmail.com>
2017-09-21 21:42:51 +0200

    Remove primary key from login sql

--- src/main/resources/db/migration/V1.03__Create_table_login.sql
+++ src/main/resources/db/migration/V1.03__Create_table_login.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS `login` (
   `login`	TEXT UNIQUE NOT NULL,
-  `password`	TEXT NOT NULL,
-  FOREIGN KEY (`login`) REFERENCES `user`(`login`) ON DELETE SET NULL
+  `password`	TEXT NOT NULL
 );
 INSERT INTO `login` VALUES ('admin','admin');
