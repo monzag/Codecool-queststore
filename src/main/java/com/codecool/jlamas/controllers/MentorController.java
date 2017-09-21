4c53701db19ed9f6f58895260ef4fff94423261e
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-09-21 10:06:26 +0200

    Add MentorView as class attr

--- src/main/java/com/codecool/jlamas/controllers/MentorController.java
+++ src/main/java/com/codecool/jlamas/controllers/MentorController.java
@@ -1,7 +1,19 @@
 package com.codecool.jlamas.controllers;
 
+import com.codecool.jlamas.models.account.Mentor;
+
+import com.codecool.jlamas.views.MentorView;
+
 
 public class MentorController {
 
-    
+    private Mentor user;
+    private MentorView view;
+
+    public MentorController(Mentor user) {
+        this.user = user;
+        this.view =  new MentorView();
+    }
+
+
 }
