3ae7ba957fbfc0ff07c1907f1a51e78d313ec5c9
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-09-21 14:28:01 +0200

    Remove methods moved to Codecooler view

--- src/main/java/com/codecool/jlamas/views/MentorView.java
+++ src/main/java/com/codecool/jlamas/views/MentorView.java
@@ -5,38 +5,8 @@ import java.util.Scanner;
 
 public class MentorView extends CodecoolerView {
 
-    private Scanner input;
-
     public MentorView() {
-        this.input = new Scanner(System.in);
-    }
-
-    private void printMenu() {
-        String output;
-
-        output = "\nMENU";
-        for (int i = 0; i < MENU.length; i++) {
-            output += String.format("  %d) %s.\n", i+1, MENU[i]);
-        }
-        output += "  0) Exit.\n";
-
-        System.out.println(output);
-    }
-
-    public String getMenuOption() {
-        Integer option;
-
-        this.printMenu();
-        System.out.print("Choose option: ");
-
-        try {
-            option = Integer.parseInt(this.input.nextLine());
-            return MENU[option-1];
-        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-
-        }
-
-        return "";
+        super();
     }
 
 
