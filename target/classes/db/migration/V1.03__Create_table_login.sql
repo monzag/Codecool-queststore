d7a8ccf5a297303b4a69be8a5b5044b7b498f3bd
monzag <zaganiacz.m@gmail.com>
2017-09-22 08:50:00 +0200

    Remove target

--- target/classes/db/migration/V1.03__Create_table_login.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `login` (
-  `login`	TEXT UNIQUE NOT NULL,
-  `password`	TEXT NOT NULL
-);
-INSERT INTO `login` VALUES ('admin','admin');
-INSERT INTO `login` VALUES ('mentor','mentor');
