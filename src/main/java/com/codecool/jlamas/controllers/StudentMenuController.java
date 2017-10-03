7f2e3acff60726d307dce9fbf5f4c3395d801770
monzag <zaganiacz.m@gmail.com>
2017-10-03 11:47:10 +0200

    Add static fields to switch function in start method

--- src/main/java/com/codecool/jlamas/controllers/StudentMenuController.java
+++ src/main/java/com/codecool/jlamas/controllers/StudentMenuController.java
@@ -4,6 +4,15 @@ import com.codecool.jlamas.models.account.Student;
 
 public class StudentMenuController {
 
+    public static final String[] MENU = {"Display wallet",
+                                         "Buy artifact",
+                                         "Display level"};
+
+    public static final String DISPLAY_WALLET = "1";
+    public static final String BUY_ARTIFACT = "2";
+    public static final String DISPLAY_LEVEL = "3";
+    
+
     Student student;
 
     public StudentMenuController(Student student) {
