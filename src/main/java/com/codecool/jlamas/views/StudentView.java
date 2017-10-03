5272d84258b2410a80c668a11fb0a281e2dc5cbb
monzag <zaganiacz.m@gmail.com>
2017-10-03 10:34:37 +0200

    Refactor StudentView

--- src/main/java/com/codecool/jlamas/views/StudentView.java
+++ src/main/java/com/codecool/jlamas/views/StudentView.java
@@ -2,7 +2,6 @@ package com.codecool.jlamas.views;
 
 import java.util.ArrayList;
 
-import com.codecool.jlamas.exceptions.InvalidUserDataException;
 import com.codecool.jlamas.models.account.Student;
 
 public class StudentView extends CodecoolerView {
