365f99f2229e7b8520953dc2db325d2633ef3f90
monzag <zaganiacz.m@gmail.com>
2017-10-03 11:35:16 +0200

    Add StudentMenuController

---
+++ src/main/java/com/codecool/jlamas/controllers/StudentMenuController.java
@@ -0,0 +1,16 @@
+package com.codecool.jlamas.controllers;
+
+import com.codecool.jlamas.models.account.Student;
+
+public class StudentMenuController {
+
+    Student student;
+
+    public StudentMenuController(Student student) {
+        this.student = student;
+    }
+
+    public void start() {
+        
+    }
+}
