f8f402187ba2e7e327035c73285817ca064968cd
monzag <zaganiacz.m@gmail.com>
2017-10-03 13:48:21 +0200

    Add Group model

---
+++ src/main/java/com/codecool/jlamas/models/accountdata/Group.java
@@ -0,0 +1,22 @@
+package com.codecool.jlamas.models.accountdata;
+
+public class Group {
+
+    private String name;
+
+    public Group() {
+
+    }
+
+    public Group(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
