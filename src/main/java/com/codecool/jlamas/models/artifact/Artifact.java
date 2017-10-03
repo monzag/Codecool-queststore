e6168d9a687f8b02098fa19d4d78c7a03438e770
wutismyname <plebejoosh@gmail.com>
2017-10-03 13:34:11 +0200

    Add getters and setters to Artifact

--- src/main/java/com/codecool/jlamas/models/artifact/Artifact.java
+++ src/main/java/com/codecool/jlamas/models/artifact/Artifact.java
@@ -2,9 +2,9 @@ package com.codecool.jlamas.models.artifact;
 
 public class Artifact {
 
-    String name;
-    Integer price;
-    String description;
+    private String name;
+    private Integer price;
+    private String description;
 
     public Artifact() {
 
@@ -16,5 +16,27 @@ public class Artifact {
         this.description = description;
     }
 
+    public String getName() {
+        return this.name;
+    }
 
+    public Integer getPrice() {
+        return this.price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
