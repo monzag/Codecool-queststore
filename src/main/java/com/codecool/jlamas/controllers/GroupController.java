baf8877d34cf3dce4fbcfcb2780381b682881007
monzag <zaganiacz.m@gmail.com>
2017-10-03 14:30:38 +0200

    Add createGroup

--- src/main/java/com/codecool/jlamas/controllers/GroupController.java
+++ src/main/java/com/codecool/jlamas/controllers/GroupController.java
@@ -1,8 +1,22 @@
 package com.codecool.jlamas.controllers;
 
+import com.codecool.jlamas.database.GroupDAO;
+import com.codecool.jlamas.models.accountdata.Group;
+import com.codecool.jlamas.views.GroupTeamView;
+
 public class GroupController {
 
+    private GroupTeamView groupView;
+    private GroupDAO groupDAO;
+
     public GroupController() {
-        
+        this.groupView = new GroupTeamView();
+        this.groupDAO = new GroupDAO();
+    }
+
+    public void createGroup() {
+        String name = groupView.getString("\nType name of new group: ");
+        Group group = new Group(name);
+        groupDAO.insertGroup(group);
     }
 }
