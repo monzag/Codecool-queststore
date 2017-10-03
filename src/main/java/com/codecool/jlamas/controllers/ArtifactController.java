8b3eb94f037025cb54e51f1aa6b7c82ed0ace2fe
wutismyname <plebejoosh@gmail.com>
2017-10-03 14:45:57 +0200

    Add displayAttribute() to ArtifactView

--- src/main/java/com/codecool/jlamas/controllers/ArtifactController.java
+++ src/main/java/com/codecool/jlamas/controllers/ArtifactController.java
@@ -16,6 +16,34 @@ public class ArtifactController {
     }
 
     public void editArtifact() {
-
+        try {
+            Artifact artifact = chooseArtifact();
+            artifactView.displayAttribute();
+            String option = mentorView.getString("Your choice: ");
+            switch(option) {
+                case EDIT_NAME:
+                    String name = mentorView.getName();
+                    mentor.setName(name);
+                    break;
+                case EDIT_SURNAME:
+                    String surname = mentorView.getSurname();
+                    mentor.setSurname(surname);
+                    break;
+                case EDIT_EMAIL:
+                    Mail email = mentorView.getMail();
+                    mentor.setEmail(email);
+                    break;
+                case EDIT_PASSWORD:
+                    String passwordText = mentorView.getString("New password: ");
+                    mentor.setPassword(new Password(passwordText));
+                    break;
+                default: mentorView.printErrorMessage();
+                    break;
+            }
+            mentorDao.update(mentor);
+        } catch (IndexOutOfBoundsException|InvalidUserDataException e) {
+            e.getMessage();
+        }
     }
 }
+
--- src/main/java/com/codecool/jlamas/views/ArtifactView.java
+++ src/main/java/com/codecool/jlamas/views/ArtifactView.java
@@ -18,4 +18,23 @@ public class ArtifactView {
             i++;
         }
     }
+
+    public void displayAttribute() {
+        String[] attributes = {"name",
+                "price",
+                "description"};
+        printMenu(attributes);
+    }
+
+    public void printMenu(String[] options) {
+        String output;
+
+        output = "\nMENU\n";
+        for (int i = 0; i < options.length; i++) {
+            output += String.format("  %d) %s.\n", i+1, options[i]);
+        }
+        output += "  0) Exit.\n";
+
+        System.out.println(output);
+    }
 }
