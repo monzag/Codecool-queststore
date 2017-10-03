6865ed3041580efad2e0f6d6fb0ef7242f3b4b1a
monzag <zaganiacz.m@gmail.com>
2017-10-03 14:25:43 +0200

    Add getString

--- src/main/java/com/codecool/jlamas/views/GroupTeamView.java
+++ src/main/java/com/codecool/jlamas/views/GroupTeamView.java
@@ -3,6 +3,15 @@ package com.codecool.jlamas.views;
 public class GroupTeamView {
 
     public GroupTeamView() {
-        
+
+    }
+
+    public String getString(String msg) {
+        String userInput;
+
+        System.out.print("\n" + msg + ": ");
+        userInput = System.console().readLine();
+
+        return userInput;
     }
 }
