70f509428840401b5846161cf422fced07a98e71
monzag <zaganiacz.m@gmail.com>
2017-10-03 13:59:58 +0200

    Add insertGroup

--- src/main/java/com/codecool/jlamas/database/GroupDAO.java
+++ src/main/java/com/codecool/jlamas/database/GroupDAO.java
@@ -1,9 +1,27 @@
 package com.codecool.jlamas.database;
 
+import java.sql.*;
+
+import com.codecool.jlamas.models.accountdata.Group;
+
 public class GroupDAO {
 
     public GroupDAO() {
 
+    }
+
+    public void insertGroup(Group group) {
+        String query = "INSERT INTO group(name) VALUES (?);";
+
+        try (Connection c = ConnectDB.connect();
+            PreparedStatement pstmt = c.prepareStatement(query);) {
+
+            pstmt.setString(1, group.getName());
+            pstmt.executeUpdate();
+
+        } catch (ClassNotFoundException|SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
