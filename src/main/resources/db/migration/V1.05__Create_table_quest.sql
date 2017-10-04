c3a9392194679787e9137a5df49e25f35c710bac
wutismyname <plebejoosh@gmail.com>
2017-10-04 18:53:54 +0200

    Remove date and id from DoneQuest sql table

--- src/main/resources/db/migration/V1.05__Create_table_quest.sql
+++ src/main/resources/db/migration/V1.05__Create_table_quest.sql
@@ -2,16 +2,13 @@ CREATE TABLE IF NOT EXISTS `quest` (
 	`name`	TEXT NOT NULL,
   `description`	TEXT,
 	`reward`	INTEGER,
-	PRIMARY KEY(`Name`)
+	PRIMARY KEY(`name`)
 );
 
 CREATE TABLE IF NOT EXISTS `done_quest` (
-  `id`	TEXT NOT NULL,
   `quest_name`	TEXT,
   `owner_name`	TEXT,
-  `date`	TEXT,
   FOREIGN KEY(`quest_name`) REFERENCES `quest`(`name`) ON DELETE SET NULL,
-  PRIMARY KEY(`id`),
   FOREIGN KEY(`owner_name`) REFERENCES `student`(`name`) ON DELETE SET NULL
 );
 
