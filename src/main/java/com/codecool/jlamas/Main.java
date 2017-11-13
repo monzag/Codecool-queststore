c2dc57c9657274fc233da34efe067834c40e6f1f
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-11-13 14:50:00 +0100

    Add server init to main

--- src/main/java/com/codecool/jlamas/Main.java
+++ src/main/java/com/codecool/jlamas/Main.java
@@ -1,16 +1,25 @@
 package com.codecool.jlamas;
 
 import org.flywaydb.core.Flyway;
-import com.codecool.jlamas.controllers.AppController;
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        // set database migration
         Flyway flyway = new Flyway();
         flyway.setDataSource("jdbc:sqlite:./target/database", "jlamas", null);
         flyway.migrate();
 
-        AppController ctrl = new AppController();
-        ctrl.login();
+        // create local server
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        // server.createContext("/url", new ControllerName());
+        // server.createContext("/static", new Static());
+        server.setExecutor(null);
+
+        server.start();
+
     }
 }
