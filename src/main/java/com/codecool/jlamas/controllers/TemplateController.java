f9973eacc0beec6fd862a8c3123b1ee5935b8b5d
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-11-14 12:29:52 +0100

    Add logic for template to be displayed

--- src/main/java/com/codecool/jlamas/controllers/TemplateController.java
+++ src/main/java/com/codecool/jlamas/controllers/TemplateController.java
@@ -10,7 +10,21 @@ import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
 public class TemplateController implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
 
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/template.twig");
+        JtwigModel model = JtwigModel.newModel();
+
+        // profile pic found by login
+        model.with("login", "student");
+
+        String response = template.render(model);
+
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
 
     }
 }
