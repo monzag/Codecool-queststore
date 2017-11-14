a087102fecaf6ef8d2023e51c3c5b308e84c2c93
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-11-14 12:27:55 +0100

    Add route to Template in main

--- src/main/java/com/codecool/jlamas/Main.java
+++ src/main/java/com/codecool/jlamas/Main.java
@@ -1,5 +1,6 @@
 package com.codecool.jlamas;
 
+import com.codecool.jlamas.controllers.TemplateController;
 import org.flywaydb.core.Flyway;
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
@@ -8,16 +9,16 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         // set database migration
-        Flyway flyway = new Flyway();
-        flyway.setDataSource("jdbc:sqlite:./target/database", "jlamas", null);
-        flyway.migrate();
+//        Flyway flyway = new Flyway();
+//        flyway.setDataSource("jdbc:sqlite:./target/database", "jlamas", null);
+//        flyway.migrate();
 
         // create local server
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
         // set routes
         // server.createContext("/url", new ControllerName());
-
+        server.createContext("/template", new TemplateController());
         server.createContext("/static", new Static());
         server.setExecutor(null);
 
