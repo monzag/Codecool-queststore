3d91674659aaef02264e3e868d3803f72a1d2138
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-11-14 12:28:59 +0100

    Add Class with logic for jtwig template

---
+++ src/main/java/com/codecool/jlamas/controllers/TemplateController.java
@@ -0,0 +1,16 @@
+package com.codecool.jlamas.controllers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
+public class TemplateController implements HttpHandler {
+
+
+    }
+}
