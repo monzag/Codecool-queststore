8824c9910d9cd18da8768944729998aef7fccedc
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-11-15 19:51:37 +0100

    Change removeMentor method to fit web design

--- src/main/java/com/codecool/jlamas/controllers/MentorController.java
+++ src/main/java/com/codecool/jlamas/controllers/MentorController.java
@@ -15,11 +15,6 @@ import com.codecool.jlamas.views.MentorView;
 
 public class MentorController {
 
-    private static final String EDIT_NAME = "1";
-    private static final String EDIT_SURNAME = "2";
-    private static final String EDIT_EMAIL = "3";
-    private static final String EDIT_PASSWORD = "4";
-
     private MentorView mentorView = new MentorView();
     private MentorDAO mentorDao = new MentorDAO();
 
@@ -90,47 +85,41 @@ public class MentorController {
         return group;
     }
 
-    public void editMentor() {
-        try {
-            Mentor mentor = chooseMentor();
-            mentorView.displayAttribute();
-            String option = mentorView.getString("Your choice: ");
+//    public void editMentor() {
+//        try {
+//            Mentor mentor = chooseMentor();
+//            mentorView.displayAttribute();
+//            String option = mentorView.getString("Your choice: ");
+//
+//            switch(option) {
+//                case EDIT_NAME:
+//                    String name = mentorView.getName();
+//                    mentor.setName(name);
+//                    break;
+//                case EDIT_SURNAME:
+//                    String surname = mentorView.getSurname();
+//                    mentor.setSurname(surname);
+//                    break;
+//                case EDIT_EMAIL:
+//                    Mail email = mentorView.getMail();
+//                    mentor.setEmail(email);
+//                    break;
+//                case EDIT_PASSWORD:
+//                    String passwordText = mentorView.getString("New password: ");
+//                    mentor.setPassword(new Password(passwordText));
+//                    break;
+//                default: mentorView.printErrorMessage();
+//                    break;
+//            }
+//
+//            mentorDao.update(mentor);
+//        } catch (IndexOutOfBoundsException|InvalidUserDataException e) {
+//            e.getMessage();
+//        }
+//    }
 
-            switch(option) {
-                case EDIT_NAME: 
-                    String name = mentorView.getName();
-                    mentor.setName(name);
-                    break;
-                case EDIT_SURNAME: 
-                    String surname = mentorView.getSurname();
-                    mentor.setSurname(surname);
-                    break;
-                case EDIT_EMAIL: 
-                    Mail email = mentorView.getMail(); 
-                    mentor.setEmail(email);
-                    break;
-                case EDIT_PASSWORD: 
-                    String passwordText = mentorView.getString("New password: ");
-                    mentor.setPassword(new Password(passwordText));
-                    break;
-                default: mentorView.printErrorMessage();
-                    break;
-            }
-
-            mentorDao.update(mentor);
-        } catch (IndexOutOfBoundsException|InvalidUserDataException e) {
-            e.getMessage();
-        }   
-    }
-
-    public void removeMentor() {
-        try {
-            Mentor mentor = chooseMentor();
-            mentorDao.delete(mentor);
-
-        } catch (IndexOutOfBoundsException e) {
-            e.getMessage();
-        }
+    public void removeMentor(String login) {
+        mentorDao.delete(this.getMentor(login));
     }
 
     public Mentor chooseMentor() throws IndexOutOfBoundsException {
