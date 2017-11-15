a83ec7c6b5f01e20bb4bfb8af34963820714aef6
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-11-15 11:22:52 +0100

    Add requested routes for admin

--- src/main/java/com/codecool/jlamas/controllers/AdminController.java
+++ src/main/java/com/codecool/jlamas/controllers/AdminController.java
@@ -10,62 +10,53 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class AdminController implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
+        String response = "";
+        String method = httpExchange.getRequestMethod();
+        if (method.equals("GET")) {
+
+            if (httpExchange.getRequestURI().getPath().equals("/admin/mentors/list")) {
+                response = "";
+            }
+            else if (httpExchange.getRequestURI().getPath().equals("/admin/mentors/list/edit")) {
+                response = "";
+            }
+            else if (httpExchange.getRequestURI().getPath().equals("/admin/groups/list/remove")) {
+                response = "";
+            }
+            else if (httpExchange.getRequestURI().getPath().equals("/admin/mentors/add")) {
+                response = "";
+            }
+            else if (httpExchange.getRequestURI().getPath().equals("/admin/groups/list")) {
+                response = "";
+            }
+            else if (httpExchange.getRequestURI().getPath().equals("/admin/groups/list/edit")) {
+                response = "";
+            }
+            else if (httpExchange.getRequestURI().getPath().equals("/admin/groups/add")) {
+                response = "";
+            }
+            else if (httpExchange.getRequestURI().getPath().equals("/admin/groups/list/remove")) {
+                response = "";
+            }
+        }
+
+        if (method.equals("POST")) {
+
+        }
+
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 
-//    public static final String[] OPTIONS = {"Display mentors",
-//                                            "Add mentor",
-//                                            "Edit mentor",
-//                                            "Add group",
-//                                            "Add level",
-//                                            "Edit quest"};
-//
-//    private static final int DISPLAY_MENTORS = 1;
-//    private static final int ADD_MENTOR = 2;
-//    private static final int EDIT_MENTOR = 3;
-//    private static final int ADD_GROUP = 4;
-//    private static final int ADD_LEVEL = 5;
-//    private static final int EDIT_QUEST = 6;
-//    private static final int EXIT = 0;
-//
-//    private Admin admin;
-//    private AdminView adminView = new AdminView();;
-//    private MentorController mentorController = new MentorController();
-//    ArrayList<Mentor> mentors;
-//
-//    public AdminController(Admin admin) {
-//        this.admin = admin;
-//        this.adminView = new AdminView();
-//        this.mentorController = new MentorController();
-//    }
-//
-//    public void start() {
-//        Integer option = 1;
-//        while (!option.equals(EXIT)) {
-//            adminView.printMenu(OPTIONS);
-//            option = adminView.getMenuOption();
-//
-//            switch(option) {
-//                case DISPLAY_MENTORS: displayAllMentors();
-//                    break;
-//                case ADD_MENTOR: createMentor();
-//                    break;
-//                case EDIT_MENTOR: editMentor();
-//                    break;
-//                case ADD_GROUP: addGroup();
-//                    break;
-//                case ADD_LEVEL: addLevel();
-//                    break;
-//                case EDIT_QUEST: editQuest();
-//                    break;
-//            }
-//        }
-//    }
-//
+
 //    public void displayAllMentors() {
 //        mentorController.displayMentors();
 //    }
