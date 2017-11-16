3b99d484805ecf5a73cd701e4e8f57f63e948636
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-11-16 10:36:53 +0100

    Changed group_tag in mentor table to reference group table

--- src/main/resources/db/migration/V1.01__Create_table_mentor.sql
+++ src/main/resources/db/migration/V1.01__Create_table_mentor.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS `mentor` (
 	`login`	TEXT NOT NULL UNIQUE,
-	`group_tag` TEXT NOT NULL
+	`group_tag` TEXT NOT NULL,
+	FOREIGN KEY (`group_tag`) REFERENCES `group`(`group_tag`) ON DELETE SET NULL
 );
 INSERT INTO `mentor` VALUES ('mentor','2017.1');
