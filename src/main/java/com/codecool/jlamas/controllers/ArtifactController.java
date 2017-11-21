eb3ba76f944a7f27f587c525ad525adda3c6ec86
monzag <zaganiacz.m@gmail.com>
2017-11-21 14:44:54 +0100

    Refactor ArtifactController - remove useless fields and imports

--- src/main/java/com/codecool/jlamas/controllers/ArtifactController.java
+++ src/main/java/com/codecool/jlamas/controllers/ArtifactController.java
@@ -4,18 +4,12 @@ import com.codecool.jlamas.database.ArtifactDAO;
 import com.codecool.jlamas.database.OwnedArtifactDAO;
 import com.codecool.jlamas.database.StudentDAO;
 import com.codecool.jlamas.models.artifact.Artifact;
-import com.codecool.jlamas.views.ArtifactView;
 
 import java.util.ArrayList;
 
 public class ArtifactController {
 
-    private static final String EDIT_NAME = "1";
-    private static final String EDIT_PRICE = "2";
-    private static final String EDIT_DESCRIPTION = "3";
-
     private ArtifactDAO artifactDao = new ArtifactDAO();
-    private ArtifactView artifactView = new ArtifactView();
     private OwnedArtifactDAO ownedArtifactDAO = new OwnedArtifactDAO();
     private StudentDAO studentDAO = new StudentDAO();
 
