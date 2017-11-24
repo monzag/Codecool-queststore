c753704d254b59f5a99fa9bdfa032488a309b43b
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-11-24 15:58:21 +0100

    Adapt to new group model

--- src/main/java/com/codecool/jlamas/controllers/CityController.java
+++ src/main/java/com/codecool/jlamas/controllers/CityController.java
@@ -4,9 +4,14 @@ import com.codecool.jlamas.database.CityDAO;
 import com.codecool.jlamas.models.accountdata.City;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 
 public class CityController {
+
+    // decide which years beside current will be displayed
+    private static final int PAST_YEARS = 1;
+    private static final int NEXT_YEARS = 4;
 
     private CityDAO dao;
 
@@ -16,6 +21,16 @@ public class CityController {
 
     public ArrayList<City> getAll() {
         return this.dao.requestAll();
+    }
+
+    public ArrayList<Integer> getYears() {
+        ArrayList<Integer> years = new ArrayList<Integer>();
+
+        Integer year = Calendar.getInstance().get(Calendar.YEAR);
+        for (int i = year-PAST_YEARS; i < year+NEXT_YEARS; i++) {
+            years.add(i);
+        }
+        return years;
     }
 
 }
