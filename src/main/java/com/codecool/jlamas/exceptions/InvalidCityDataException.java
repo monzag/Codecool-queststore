ddfe90257b7949f0d27d1cb7843e1ff7d0607d1d
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-11-27 13:43:27 +0100

    Add custom exceptions reporting erros in edit/add new city

---
+++ src/main/java/com/codecool/jlamas/exceptions/InvalidCityDataException.java
@@ -0,0 +1,10 @@
+package com.codecool.jlamas.exceptions;
+
+
+public class InvalidCityDataException extends Exception {
+
+    public InvalidCityDataException(String message) {
+        super(message);
+    }
+
+}
