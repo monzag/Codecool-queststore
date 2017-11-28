44fd7fedd7dc034de4a9bc17528d574cff120d5f
monzag <zaganiacz.m@gmail.com>
2017-11-28 15:38:09 +0100

    Move addCookieToDB and getLoginByCookie from CookieController to SessionDAO

--- src/main/java/com/codecool/jlamas/controllers/CookieController.java
+++ src/main/java/com/codecool/jlamas/controllers/CookieController.java
@@ -1,13 +1,18 @@
 package com.codecool.jlamas.controllers;
 
-import com.codecool.jlamas.database.ConnectDB;
+import com.codecool.jlamas.database.SessionDAO;
 import com.sun.net.httpserver.HttpExchange;
 
 import java.net.HttpCookie;
-import java.sql.*;
 import java.util.UUID;
 
 public class CookieController {
+
+    private SessionDAO session;
+
+    public CookieController() {
+        this.session = new SessionDAO();
+    }
 
     public HttpCookie getCookie(HttpExchange httpExchange) {
         String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
@@ -24,45 +29,8 @@ public class CookieController {
         String sessionId = UUID.randomUUID().toString();
         HttpCookie cookie = new HttpCookie("sessionId", sessionId);
         httpExchange.getResponseHeaders().add("Set-Cookie", cookie.toString());
-        this.addCookieToDb(cookie, login);
+        session.addCookieToDb(cookie, login);
 
         return cookie;
-    }
-
-    public void addCookieToDb(HttpCookie cookie, String login) {
-        String query = "INSERT INTO `cookie` VALUES (?, ?);";
-
-        try (Connection c = ConnectDB.connect();
-             PreparedStatement pstmt = c.prepareStatement(query)) {
-
-            pstmt.setString(1, cookie.getValue());
-            pstmt.setString(2, login);
-            pstmt.executeUpdate();
-
-        } catch (ClassNotFoundException|SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-    }
-
-    public String getLoginByCookie(HttpExchange httpExchange) {
-        String login = "";
-        HttpCookie cookie = getCookie(httpExchange);
-        String query = "SELECT login FROM `cookie` WHERE sessionId = '" + cookie.getValue() + "';";
-
-        try (Connection c = ConnectDB.connect();
-             Statement stmt = c.createStatement()) {
-
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-                login = rs.getString("login");
-            }
-
-        } catch (ClassNotFoundException|SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-        return login;
     }
 }
