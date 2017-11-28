44d778ffa737f40fff10b382c9c218a1aa589f90
monzag <zaganiacz.m@gmail.com>
2017-11-28 12:00:45 +0100

    Move cookies methods to from AppController to CookieController

--- src/main/java/com/codecool/jlamas/controllers/CookieController.java
+++ src/main/java/com/codecool/jlamas/controllers/CookieController.java
@@ -1,4 +1,50 @@
 package com.codecool.jlamas.controllers;
 
+import com.codecool.jlamas.database.ConnectDB;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.net.HttpCookie;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
+
 public class CookieController {
+
+    public HttpCookie getCookie(HttpExchange httpExchange) {
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+        HttpCookie cookie = null;
+        if (cookieStr != null) {  // Cookie already exists
+            cookie = HttpCookie.parse(cookieStr).get(0);
+        }
+
+        return cookie;
+    }
+
+    public HttpCookie createCookie(HttpExchange httpExchange, String login) {
+        // Create a new cookie
+        String sessionId = UUID.randomUUID().toString();
+        HttpCookie cookie = new HttpCookie("sessionId", sessionId);
+        httpExchange.getResponseHeaders().add("Set-Cookie", cookie.toString());
+        this.addCookieToDb(cookie, login);
+
+        return cookie;
+    }
+
+    public void addCookieToDb(HttpCookie cookie, String login) {
+        String query = "INSERT INTO `cookie` VALUES (?, ?);";
+
+        try (Connection c = ConnectDB.connect();
+             PreparedStatement pstmt = c.prepareStatement(query)) {
+
+            pstmt.setString(1, cookie.getValue());
+            pstmt.setString(2, login);
+            pstmt.executeUpdate();
+
+        } catch (ClassNotFoundException|SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
 }
