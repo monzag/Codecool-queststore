72992cf95c65cf84bb4292e5c29c44aef92ae307
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-11-29 11:43:21 +0100

    Rename mentormenucontroller to mentorHandler

--- src/main/java/com/codecool/jlamas/controllers/MentorHandler.java
+++ src/main/java/com/codecool/jlamas/controllers/MentorHandler.java
@@ -19,7 +19,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
-public class MentorMenuController implements HttpHandler{
+public class MentorHandler implements HttpHandler{
 
     private StudentController studentController = new StudentController();
     private QuestController questController = new QuestController();
