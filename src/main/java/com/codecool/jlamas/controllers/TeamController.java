a8350372999f3813e37bb97a026a2b4bf4233e5b
wutismyname <plebejoosh@gmail.com>
2017-11-30 23:13:20 +0100

    Add cityController

---
+++ src/main/java/com/codecool/jlamas/controllers/TeamController.java
@@ -0,0 +1,36 @@
+package com.codecool.jlamas.controllers;
+
+import com.codecool.jlamas.database.TeamDAO;
+import com.codecool.jlamas.models.accountdata.Team;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class TeamController extends Controller<Team> {
+
+    private TeamDAO teamDao;
+
+    public TeamController() {
+        this.teamDao = new TeamDAO();
+    }
+
+    public Team get(String id) {
+        return teamDao.get(Integer.valueOf(id));
+    }
+
+    public ArrayList<Team> getAll() {
+        return teamDao.getAll();
+    };
+
+    public void remove(String id) {
+        teamDao.delete(Integer.valueOf(id));
+    };
+
+    public void createFromMap(Map<String, String> attrs) throws Exception {
+
+    };
+
+    public void editFromMap(Map<String, String> attrs, String login) throws Exception{
+
+    };
+}
