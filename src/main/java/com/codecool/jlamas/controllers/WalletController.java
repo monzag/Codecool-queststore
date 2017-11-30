93cec79caef686a9c263b47a80ca1baf074338ef
wutismyname <plebejoosh@gmail.com>
2017-11-30 17:23:04 +0100

    Remove unused imports and methods

--- src/main/java/com/codecool/jlamas/controllers/WalletController.java
+++ src/main/java/com/codecool/jlamas/controllers/WalletController.java
@@ -10,19 +10,9 @@ public class WalletController {
 
     Student student;
     private StudentDAO studentDAO = new StudentDAO();
-    private ArtifactDAO artifactDAO = new ArtifactDAO();
-    private DoneQuestDAO doneQuestsDAO = new DoneQuestDAO();
     private OwnedArtifactDAO ownedArtifactDAO = new OwnedArtifactDAO();
-    private TeamPurchaseDAO teamPurchaseDAO = new TeamPurchaseDAO();
-    private ArtifactController artifactController = new ArtifactController();
-
     public WalletController(Student student) {
         this.student = student;
-    }
-
-    public void addDoneQuest(Quest quest) {
-        this.student.getWallet().getDoneQuests().add(quest);
-        doneQuestsDAO.insert(this.student, quest);
     }
 
     public boolean buyArtifact(Artifact artifact) {
@@ -35,13 +25,6 @@ public class WalletController {
             System.out.println(e.getMessage());
         }
         return false;
-    }
-
-
-
-    public void addTeamPurchase(TeamPurchase purchase) {
-        this.student.getWallet().getPendingPurchases().add(purchase);
-        teamPurchaseDAO.insert(purchase);
     }
 
     public boolean addOwnedArtifact(Artifact artifact) {
