0cce1bd2b17efe1550ad6c3590c101dd7a790f06
wutismyname <plebejoosh@gmail.com>
2017-11-30 21:18:18 +0100

    Add team create table sql

--- src/main/resources/db/migration/V1.02__Create_table_student.sql
+++ src/main/resources/db/migration/V1.02__Create_table_student.sql
@@ -1,11 +1,20 @@
+CREATE TABLE IF NOT EXISTS `team` (
+	`id` INTEGER AUTOINCREMENT UNIQUE NOT NULL,
+	`name` TEXT,
+	PRIMARY KEY (`id`)
+);
+
+INSERT INTO `team` VALUES (null, 'Zgrywusy');
+
 CREATE TABLE IF NOT EXISTS `student` (
 	`login`	TEXT NOT NULL UNIQUE,
 	`group_id`	INTEGER,
-	`team_tag`	INTEGER NOT NULL,
+	`team`	INTEGER,
 	`balance` INTEGER NOT NULL,
 	FOREIGN KEY (`login`) REFERENCES `user`(`login`) ON DELETE SET NULL,
-	FOREIGN KEY (`group_id`) REFERENCES `group`(`id`) ON DELETE SET NULL
+	FOREIGN KEY (`group_id`) REFERENCES `group`(`id`) ON DELETE SET NULL,
+	FOREIGN KEY (`team`) REFERENCES `team`(`id`) ON DELETE SET NULL
 );
 
-INSERT INTO `student` VALUES ('student', 1, 'Guwniaks','0');
+INSERT INTO `student` VALUES ('student', 1, 1,'0');
 
