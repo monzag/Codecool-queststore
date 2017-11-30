5a44ef6b6d3ee4004990ca57b336b4f8f5019a22
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-11-30 12:41:55 +0100

    Add create, delete and select methods in level controller

---
+++ src/main/java/com/codecool/jlamas/controllers/LevelController.java
@@ -0,0 +1,37 @@
+package com.codecool.jlamas.controllers;
+
+import com.codecool.jlamas.database.LevelDAO;
+import com.codecool.jlamas.models.level.Level;
+
+import java.util.List;
+
+public class LevelController {
+    private LevelDAO levelDAO;
+
+    public LevelController() {
+    }
+
+    public LevelController(LevelDAO levelDAO) {
+        this.levelDAO = levelDAO;
+    }
+
+    public void editLevel(String oldName, Level level) {
+        levelDAO.updateLevel(level, oldName);
+    }
+
+    public void createLevel(Level level) {
+        levelDAO.insertLevel(level);
+    }
+
+    public void deleteLevel(Level level) {
+        levelDAO.deleteLevel(level);
+    }
+
+    public List<Level> showAllLevels() {
+        return levelDAO.selectAll();
+    }
+
+    public Level chooseLevel(String levelName) {
+
+    }
+}
