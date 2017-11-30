fb16fda992d1c579e305e4e8333d6e573b4e6f7d
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-11-30 23:57:44 +0100

    Delete unsused methods

--- src/main/java/com/codecool/jlamas/controllers/QuestController.java
+++ src/main/java/com/codecool/jlamas/controllers/QuestController.java
@@ -71,27 +71,9 @@ public class QuestController implements Controller<Quest> {
         return true;
     }
 
-    @Deprecated
-    public void createQuest(Quest quest) {
-
-        this.questDAO.insertQuest(quest);
-
-    }
-
-    @Deprecated
-    public void deleteQuest(Quest quest) {
-        questDAO.deleteQuest(quest);
-    }
-
     public void markQuestAsDone(Student student, Quest quest) {
         doneQuestDAO.insert(student, quest);
         student.getWallet().put(quest.getReward());
         studentDAO.update(student);
-    }
-
-    @Deprecated
-    public Quest chooseQuest(String questName) {
-        Quest quest = questDAO.selectQuest(questName);
-        return quest;
     }
 }
