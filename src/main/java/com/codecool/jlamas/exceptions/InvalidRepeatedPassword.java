27b3f09aa02193f5515da89e4415a3254c03df54
monzag <zaganiacz.m@gmail.com>
2017-11-30 17:17:09 +0100

    Rename to InvalidRepeatedPasswordException

--- src/main/java/com/codecool/jlamas/exceptions/InvalidRepeatedPassword.java
+++
@@ -1,6 +0,0 @@
-package com.codecool.jlamas.exceptions;
-
-public class InvalidRepeatedPassword extends Exception {
-
-    public InvalidRepeatedPassword() { super("Invalid repeated password");}
-}
---
+++ src/main/java/com/codecool/jlamas/exceptions/InvalidRepeatedPasswordException.java
@@ -0,0 +1,6 @@
+package com.codecool.jlamas.exceptions;
+
+public class InvalidRepeatedPasswordException extends Exception {
+
+    public InvalidRepeatedPasswordException() { super("Invalid repeated password");}
+}
